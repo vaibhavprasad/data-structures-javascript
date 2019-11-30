1b3a6ee0df8bf291ce045d203520212b8cc06678
Vaibhav Prasad <vaibhav@acalvio.com>
2019-11-30 19:49:34 +0530

    basic singly linked list

---
+++ LinkedList.js
@@ -0,0 +1,46 @@
+class Node {
+	constructor(data, next = null) {
+		this.data = data;
+		this.next = next;
+	}
+}
+
+class LinkedList {
+	constructor () {
+		this.head = null;
+	}
+}
+
+LinkedList.prototype.insertAtBegenning = function (data) {
+	let newNode = new Node(data);
+	newNode.next = this.head;
+	this.head = newNode;
+	return this.head;
+}
+
+LinkedList.prototype.insertAtEnd = function (data) {
+	let newNode = new Node(data);
+	let lastNode = this.head;
+	while (lastNode.next !== null) {
+		lastNode = lastNode.next;
+	}
+	lastNode.next = newNode;
+}
+
+LinkedList.prototype.displayAllNodes = function () {
+	let currentNode = this.head;
+	while (currentNode) {
+		console.log(' ', currentNode.data, ' ');
+		currentNode = currentNode.next;
+	}
+}
+
+LinkedList.prototype.deleteFromBegenning = function () {
+	let deleteNode = this.head;
+	this.head = this.head.next;
+
+}
+
+LinkedList.prototype.deleteFromEnd = function () {
+	
+}
