0ff7041d6a5c5a6877277e8b049baa2429456764
Vaibhav Prasad <vaibhav@acalvio.com>
2020-03-26 15:48:22 +0530

    singly linked list completed

--- LinkedList.js
+++ LinkedList.js
@@ -1,3 +1,5 @@
+// Datastructure definition
+
 class Node {
 	constructor(data, next = null) {
 		this.data = data;
@@ -8,23 +10,8 @@ class Node {
 class LinkedList {
 	constructor () {
 		this.head = null;
+		this.size = 0;
 	}
-}
-
-LinkedList.prototype.insertAtBegenning = function (data) {
-	let newNode = new Node(data);
-	newNode.next = this.head;
-	this.head = newNode;
-	return this.head;
-}
-
-LinkedList.prototype.insertAtEnd = function (data) {
-	let newNode = new Node(data);
-	let lastNode = this.head;
-	while (lastNode.next !== null) {
-		lastNode = lastNode.next;
-	}
-	lastNode.next = newNode;
 }
 
 LinkedList.prototype.displayAllNodes = function () {
@@ -35,15 +22,62 @@ LinkedList.prototype.displayAllNodes = function () {
 	}
 }
 
+LinkedList.prototype.insertAtBegenning = function (data) {
+	let newNode = new Node(data);
+	newNode.next = this.head;
+	this.head = newNode;
+	this.size++;
+}
+
+LinkedList.prototype.insertAtEnd = function (data) {
+	let newNode = new Node(data);
+	if (this.head === null) {
+		this.head = newNode;
+	} else {
+		let lastNode = this.head;
+		while (lastNode.next !== null) {
+			lastNode = lastNode.next;
+		}
+		lastNode.next = newNode;
+	}
+	this.size++;
+}
+
 LinkedList.prototype.deleteFromBegenning = function () {
-	let deleteNode = this.head;
-	this.head = this.head.next;
-    this.deleteNode = null;
+	if (this.size !== 0) {
+		let deleteNode = this.head;
+		this.head = this.head.next;
+		this.deleteNode = null;
+		this.size--;
+	}
 }
 
 LinkedList.prototype.deleteFromEnd = function () {
-    let tempPtr = head;
-    while(this.tempPtr.next) {
-        tempPtr = tempPtr.next;
-    }
+	let tempPtr = this.head;
+	let previousPtr = null;
+	if (this.size !== 0) {
+		while(tempPtr.next) {
+			previousPtr = tempPtr;
+			tempPtr = tempPtr.next;
+		}
+		previousPtr.next = null;
+		tempPtr = null;
+		this.size--;
+	}
 }
+
+// Usage 
+
+let list = new LinkedList();
+list.insertAtBegenning(20);
+list.insertAtEnd(10);
+list.displayAllNodes();
+list.insertAtEnd(30);
+list.insertAtEnd(70);
+list.insertAtEnd(40);
+list.insertAtEnd(60);
+list.displayAllNodes();
+list.deleteFromBegenning();
+list.displayAllNodes();
+list.deleteFromEnd();
+list.displayAllNodes();
