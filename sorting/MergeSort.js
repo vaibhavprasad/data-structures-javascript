ada2e6df5d4d43df1a5b5004f199f122eef6e357
Vaibhav Prasad <vaibhav@acalvio.com>
2020-04-11 03:19:18 +0530

    merge and quick sort

---
+++ sorting/MergeSort.js
@@ -0,0 +1,51 @@
+// the following program contains two methods, one is the merge sort using recursion and another is a helper method to merge two sorted arrays
+
+function mergeSort (arr) {
+    if (arr.length === 1) {
+        return arr;
+    } else {
+        let mid = Math.ceil(arr.length / 2);
+        let a1 = mergeSort(arr.slice(0, mid));
+        let a2 = mergeSort(arr.slice(mid, arr.length));
+        return merge(a1, a2);
+    }
+}
+
+function merge (nums1, nums2) {
+    let mergedArr = [];
+    let totalLen = nums1.length + nums2.length;
+    if (nums1.length === 0) {
+        mergedArr = nums2;
+    } else if (nums2.length === 0) {
+        mergedArr = nums1;
+    } else {
+        let i = 0;
+        let j = 0;
+        let k = 0;
+        while (k <= totalLen / 2) {
+            if(nums1[i] <= nums2[j]) {
+                mergedArr.push(nums1[i])
+                i += 1;
+            } else {
+                mergedArr.push(nums2[j]);
+                j += 1;
+            }
+            if (i === nums1.length) {
+                mergedArr = mergedArr.concat(nums2.slice(j));
+                break;
+            } else if (j === nums2.length) {
+                mergedArr = mergedArr.concat(nums1.slice(i));
+                break;
+            }
+        } 
+    }
+    return mergedArr;
+}
+function genRandomArray(size) {
+    return Array.apply(null, {length: size}).map(Function.call, Math.random);
+}
+let data = genRandomArray(10000);
+let t0 = performance.now();
+let mergedData = mergeSort(data);
+let t1 = performance.now();
+console.log(t1 - t0, '\n', mergedData);
---
+++ sorting/QuickSort.js
@@ -0,0 +1,41 @@
+// The following code shows the implementation of quick sort on a random array of 100,000 elements. Its a single pivot quick sort with pivot fixed as the 1st element of the array.
+// No auxillary space has been used to the array arr is declared outside of the function scope.
+
+function quickSort(arr, l, r) { // initially l = 0, r = length - 1
+    if (l >= r) {
+        return arr;
+    }
+    let pivot = partition(arr, l , r);
+    quickSort(arr, l, pivot - 1);
+    quickSort(arr, pivot + 1, r);
+}
+
+function partition (arr, l, r) {
+    let j = l + 1;
+    let pivot = arr[l];
+    for (let i = j; i <= r; i++) {
+        if (arr[i] < pivot) {
+            if (j !== i) {
+                let tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+            }
+            j++;
+        } else {
+            continue;
+        }
+    }
+    arr[l] = arr[j - 1];
+    arr[j - 1] = pivot;
+    return j - 1;
+}
+
+function genRandomArray(size) {
+    return Array.apply(null, {length: size}).map(Function.call, Math.random);
+}
+
+let arr = genRandomArray(100000);
+let t0 = performance.now();
+quickSort(arr, 0, 7);
+let t1 = performance.now();
+console.log(t1 - t0, '\n', arr);
